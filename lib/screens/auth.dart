dac7e373d78274adf79fa592ca5a9f0861d8eb71
kurylko <reviliya@gmail.com>
2023-12-28 14:33:47 +0100

    user can sign in

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:firebase_auth/firebase_auth.dart';
+
+final _firebase = FirebaseAuth.instance;
+
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
 
@@ -17,12 +21,30 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredPassword = '';
 
 //checking if inputs are valid
-  _submit() {
+  _submit() async {
     final isValid = _form.currentState!.validate();
-    if (isValid) {
-      _form.currentState!.save();
-      print(_enteredEmail);
-      print(_enteredPassword);
+    if (!isValid) {
+      return;
+    }
+
+    _form.currentState!.save();
+
+    if (_isLogin) {
+      //log users in or create a new one
+    } else {
+      try {
+        final UserCredentials = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail, password: _enteredPassword);
+            print(UserCredentials);
+      } on FirebaseException catch (error) {
+        if (error.code == 'email-already-in-use') {
+          //...
+        }
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text(error.message ?? 'Authentification failed'),
+        ));
+      }
     }
   }
 
